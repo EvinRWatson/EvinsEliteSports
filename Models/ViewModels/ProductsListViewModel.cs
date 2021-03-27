9966dd99553cd924f56585ac4c3bd6556472ba3d
Evin Watson <evinwatson@outlook.com>
2021-03-26 19:21:26 -0700

    Initial commit

---
+++ Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using EvinsEliteSports.Models;
+namespace EvinsEliteSports.Models.ViewModels 
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# EvinsEliteSports
+ A sports store application utilzing ASP.NET MVC
