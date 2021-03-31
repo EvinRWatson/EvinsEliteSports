3d2ff55cd84612804539286cd04e1ef3348e1f53
Evin Watson <evinwatson@outlook.com>
2021-03-31 00:30:27 -0700

    Added Nav and Cart

--- obj/Debug/net5.0/Razor/Views/_ViewStart.cshtml.g.cs
+++ obj/Debug/net5.0/Razor/Views/_ViewStart.cshtml.g.cs
@@ -26,8 +26,15 @@ using EvinsEliteSports.Models.ViewModels;
 #line default
 #line hidden
 #nullable disable
+#nullable restore
+#line 3 "C:\Users\EvinW\Documents\MSSA\SportsStore\EvinsEliteSports\Views\_ViewImports.cshtml"
+using EvinsEliteSports.Infrastructure;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"7091c65830b0329e613be026ede8a57552863778", @"/Views/_ViewStart.cshtml")]
-    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"ae35ba07f1dd5b517a1a68d7646b1b12eecdcf63", @"/Views/_ViewImports.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"c561fde34f64c6a291e39d0d9a857d13d756c721", @"/Views/_ViewImports.cshtml")]
     public class Views__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
